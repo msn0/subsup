fba9e773de6cbb58b8777ddc6bd22e5d0afd67ad
Michał Jezierski <ambinanitelo@gmail.com>
2017-11-19 23:07:26 +0100

    refactor

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 'use strict';
 
-const map = {
-    sup: {
-        '0': '⁰',
-        '1': '¹',
-        '2': '²',
-        '3': '³',
-        '4': '⁴',
-        '5': '⁵',
-        '6': '⁶',
-        '7': '⁷',
-        '8': '⁸',
-        '9': '⁹',
-        '+': '⁺',
-        '-': '⁻',
-        '=': '⁼',
-        '(': '⁽',
-        ')': '⁾',
-        'n': 'ⁿ',
-        'i': 'ⁱ'
-    },
-    sub: {
-        '0': '₀',
-        '1': '₁',
-        '2': '₂',
-        '3': '₃',
-        '4': '₄',
-        '5': '₅',
-        '6': '₆',
-        '7': '₇',
-        '8': '₈',
-        '9': '₉',
-        '+': '₊',
-        '-': '₋',
-        '=': '₌',
-        '(': '₍',
-        ')': '₎'
-    }
+const superscripts = {
+    '0': '⁰',
+    '1': '¹',
+    '2': '²',
+    '3': '³',
+    '4': '⁴',
+    '5': '⁵',
+    '6': '⁶',
+    '7': '⁷',
+    '8': '⁸',
+    '9': '⁹',
+    '+': '⁺',
+    '-': '⁻',
+    '=': '⁼',
+    '(': '⁽',
+    ')': '⁾',
+    'n': 'ⁿ',
+    'i': 'ⁱ'
+};
+
+const subscripts = {
+    '0': '₀',
+    '1': '₁',
+    '2': '₂',
+    '3': '₃',
+    '4': '₄',
+    '5': '₅',
+    '6': '₆',
+    '7': '₇',
+    '8': '₈',
+    '9': '₉',
+    '+': '₊',
+    '-': '₋',
+    '=': '₌',
+    '(': '₍',
+    ')': '₎'
 };
 
 function parse(input) {
     return (input + '').split('');
 }
 
-function toSup(input) {
-    return map.sup[input];
+function to(chart) {
+    return function (input) {
+        return chart[input];
+    };
 }
 
 module.exports.sup = function (input) {
-    return parse(input).map(toSup).join('');
+    return parse(input).map(to(superscripts)).join('');
 };
 
 module.exports.sub = function (input) {
-    input = (input + '').split('');
-    return input.map(i => map.sub[i]).join('');
+    return parse(input).map(to(subscripts)).join('');
 };
